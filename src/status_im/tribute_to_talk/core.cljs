d0e6dec73bf64ca36be88d9241818a610c3bf28e
Vitaliy Vlasov <siphiuel@gmail.com>
2019-04-26 17:32:48 +0300

    Add sufficient-funds? flag to TtT transaction

--- src/status_im/tribute_to_talk/core.cljs
+++ src/status_im/tribute_to_talk/core.cljs
@@ -261,6 +261,7 @@
         chain                (keyword (:chain db))
         symbol               :STT
         all-tokens           (:wallet/all-tokens db)
+        wallet-balance       (get-in db [:wallet :balance symbol])
         {:keys [decimals]}   (tokens/asset-for all-tokens chain symbol)
         amount-text          (str snt-amount)
         {:keys [value]}      (wallet.db/parse-amount amount-text decimals)
@@ -273,6 +274,8 @@
                                 :public-key  public-key
                                 :from-chat?  true
                                 :symbol      symbol
+                                :sufficient-funds? (or (nil? snt-amount)
+                                                       (money/sufficient-funds? snt-amount wallet-balance))
                                 :amount-text amount-text
                                 :send-transaction-message? true}
                      :on-result [:tribute-to-talk.ui/on-tribute-transaction-sent
